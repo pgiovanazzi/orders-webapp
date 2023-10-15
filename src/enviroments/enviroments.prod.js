398ba254d2f223043c730bae9df43cb7c084a117
Paulo Giovanazzi <70641232+pgiovanazzi@users.noreply.github.com>
2023-10-15 15:27:19 -0300

    Update enviroments.prod.js

--- src/enviroments/enviroments.prod.js
+++ src/enviroments/enviroments.prod.js
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    pi: 'https://mcga-orders.onrender.com/api/v1.0/',
+    api: 'https://mcga-orders.onrender.com/api/v1.0/',
     tokenRefresh: 300,
     client_id: 4,
     client_secret: '',
